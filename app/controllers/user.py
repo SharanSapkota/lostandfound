e44f5078ebcf1bd38f631f0f7c85e981955ab199
Sharan Sapkota <sapkotarambbo@gmail.com>
2026-02-20 13:10:28 +0200

    Seggregate controller, service and repository

--- app/controllers/user.py
+++ app/controllers/user.py
@@ -3,37 +3,15 @@ from sqlalchemy.orm import Session
 
 from app.database import SessionLocal
 from app.models.user import User
-from app.schemas.user import UserCreate, UserResponse
-
+from app.schemas.user import UserLogin, UserResponse
+from app.dependencies.userDependency import userServiceRepoInjected
+from app.services.user import UserService
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
-
-def get_db():
-    db = SessionLocal()
+@router.post("/login", response_model=UserResponse)
+def login(payload: UserLogin, userService: UserService = Depends(userServiceRepoInjected)):
     try:
-        yield db
-    finally:
-        db.close()
-
-
-@router.post("/", response_model=UserResponse)
-def create_user(payload: UserCreate, db: Session = Depends(get_db)):
-
-    existing_user = db.query(User).filter(User.email == payload.email).first()
-    if existing_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-
-    new_user = User(
-        email=payload.email,
-        firstName=payload.firstName,
-        lastName=payload.lastName,
-        phone=payload.phone,
-        password_hash=payload.password 
-    )
-
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-
-    return new_user
+        return userService.login(payload.email, payload.password)
+    except ValueError as e:
+        raise HTTPException(status_code=401, detail=str(e))
