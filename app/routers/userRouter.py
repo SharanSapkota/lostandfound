55924d82ac82a9b235799ef2fcfaf9527623378f
Sharan Sapkota <sapkotarambbo@gmail.com>
2026-03-09 13:14:27 +0200

    Remove unwatned code

--- app/routers/userRouter.py
+++ app/routers/userRouter.py
@@ -1,5 +1,3 @@
-# app/routers/userRouter.py
-
 from fastapi import APIRouter, Depends
 from app.controllers.userController import (
     register,
@@ -13,11 +11,9 @@ from app.dependencies.authDependency import require_auth, require_admin
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
-# Public routes
 router.add_api_route("/register", register, methods=["POST"], status_code=201)
 router.add_api_route("/login", login, methods=["POST"])
 
-# Protected routes
 router.add_api_route("/", get_all_users, methods=["GET"], dependencies=[Depends(require_admin)])
 router.add_api_route("/{user_id}", get_user, methods=["GET"], dependencies=[Depends(require_auth)])
 router.add_api_route("/{user_id}", update_user, methods=["PUT"], dependencies=[Depends(require_auth)])
